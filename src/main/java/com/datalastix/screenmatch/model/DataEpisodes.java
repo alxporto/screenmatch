dbc424b20ae8bfeece456c418faf57519ce099ca
datalastix <datalastix@gmail.com>
2024-08-27 03:10:54 -0300

    create the dataepisodes record for the model package
    Signed-off-by: datalastix <datalastix@gmail.com>

---
+++ src/main/java/com/datalastix/screenmatch/model/DataEpisodes.java
@@ -0,0 +1,11 @@
+package com.datalastix.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataEpisodes(@JsonAlias("Title") String title,
+                           @JsonAlias("Episode") Integer episode,
+                           @JsonAlias("imdbRating") String ratings,
+                           @JsonAlias("Released") String released) {
+}
