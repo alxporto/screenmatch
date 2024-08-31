8ca71629afb21670e27facf5873f5eee3c22d47e
datalastix <datalastix@gmail.com>
2024-08-31 00:54:24 -0300

    build: create the episode class on model package
    Signed-off-by: datalastix <datalastix@gmail.com>

--- src/main/java/com/datalastix/screenmatch/model/Episode.java
+++ src/main/java/com/datalastix/screenmatch/model/Episode.java
@@ -1,4 +1,60 @@
 package com.datalastix.screenmatch.model;
 
+import java.time.LocalDate;
+
 public class Episode {
+  //attributes
+  private Integer seasonNumber;
+  private String  title;
+  private Integer episodeNumber;
+  private Double ratings;
+  private LocalDate releaseDate;
+
+  public Episode(Integer seasonNumber, DataEpisode dataEpisode) {
+      this.seasonNumber = seasonNumber;
+      this.title = dataEpisode.title();
+      this.episodeNumber = dataEpisode.episode();
+      this.ratings = Double.valueOf(dataEpisode.ratings());
+      this.releaseDate = LocalDate.parse(dataEpisode.releaseDate());
+  }
+
+  public Integer getSeasonNumber() {
+      return seasonNumber;
+  }
+
+  public void setSeasonNumber(Integer season) {
+      this.seasonNumber = season;
+  }
+
+  public String getTitle() {
+      return title;
+  }
+
+  public void setTitle(String title) {
+      this.title = title;
+  }
+
+  public Integer getEpisodeNumber() {
+      return episodeNumber;
+  }
+
+  public void setEpisodeNumber(Integer episodeNumber) {
+      this.episodeNumber = episodeNumber;
+  }
+
+  public Double getRatings() {
+      return ratings;
+  }
+
+  public void setRatings(Double ratings) {
+      this.ratings = ratings;
+  }
+
+  public LocalDate getReleaseDate() {
+      return releaseDate;
+  }
+
+  public void setReleaseDate(LocalDate releaseDate) {
+      this.releaseDate = releaseDate;
+  }
 }
