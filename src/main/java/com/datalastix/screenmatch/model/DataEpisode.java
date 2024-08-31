5f55a84b2787511879b7b78fc207229d02362879
datalastix <datalastix@gmail.com>
2024-08-31 00:52:31 -0300

    refactor(dataepisode): some variables names e types updates
    Signed-off-by: datalastix <datalastix@gmail.com>

--- src/main/java/com/datalastix/screenmatch/model/DataEpisode.java
+++ src/main/java/com/datalastix/screenmatch/model/DataEpisode.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DataEpisodes(@JsonAlias("Title") String title,
-                           @JsonAlias("Episode") Integer episode,
-                           @JsonAlias("imdbRating") String ratings,
-                           @JsonAlias("ReleaseDate") String releaseDate) {
+public record DataEpisode(@JsonAlias("Title") String title,
+                          @JsonAlias("Episode") Integer episodeNumber,
+                          @JsonAlias("imdbRating") String ratings,
+                          @JsonAlias("ReleaseDate") String releaseDate) {
 }
